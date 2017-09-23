6837cee9d00042aff372160b3cf33d46108ed218
Phillip Groce <pgroce@cert.org>
2017-09-23 12:16:19 -0400

    Update archive

--- docs/elpa/pg-shell-path-0.1.el
+++ docs/elpa/pg-shell-path-0.1.el
@@ -8,77 +8,76 @@
 
 (require 's)
 
-(defun pg/--shell-pathify (pathlist)
+(defun pg-shell-path--pathify (pathlist)
   "Convert a list of strings into a shell path specification."
   (if pathlist
       (cl-reduce (lambda (x y): (concat (s-trim x) ":" (s-trim y))) pathlist)
     nil))
 
 
-(defun pg/--shell-depathify-string (pathspec)
+(defun pg-shell-path--depathify-string (pathspec)
   "Convert a shell path specification PATHSPEC into a list of
 strings. If PATHSPEC is nil, returns nil."
   (if pathspec (split-string pathspec ":") nil))
 
 
-(defun pg/--shell-depathify (pathvar)
+(defun pg-shell-path--depathify (pathvar)
   "Convert the value of the environment variable PATHVAR into a
 list of strings"
-  (pg/--shell-depathify-string (getenv pathvar)))
+  (pg-shell-path--depathify-string (getenv pathvar)))
 
-(defun pg/shell-path-append (pathvar newpaths)
-  "Append list of NEWPATHS to the environment variable PATHVAR"
-  (let* ((oldpaths (pg/--shell-depathify pathvar))
-         (finalpath
-          (or (and oldpaths
-                   (pg/--shell-pathify (list
-                                        (pg/--shell-pathify oldpaths)
-                                        (pg/--shell-pathify newpaths))))
-              (pg/--shell-pathify newpaths))))
-    (setenv pathvar finalpath)))
-
-(defalias 'append-shell-path 'pg/shell-path-append)
-
-(defun pg/shell-path-prepend (pathvar newpaths)
-  "Prepend list of NEWPATHS to the environment variable PATHVAR"
-  (let* ((oldpaths (pg/--shell-depathify pathvar))
-         (finalpath
-          (or (and oldpaths
-                   (pg/--shell-pathify (list
-                                        (pg/--shell-pathify newpaths)
-                                        (pg/--shell-pathify oldpaths))))
-              (pg/--shell-pathify newpaths))))
-    (setenv pathvar finalpath)))
-
-(defalias 'prepend-shell-path 'pg/shell-path-prepend)
-
-(defun pg/shell-path-replace (pathvar newpaths)
-  "Replace PATHVAR with NEWPATHS"
-  (setenv pathvar (pg/--shell-pathify newpaths)))
-
-(defalias 'replace-shell-path 'pg/shell-path-replace)
+;;;###autoload
+  (defun pg-shell-path-append (pathvar newpaths)
+    "Append list of NEWPATHS to the environment variable PATHVAR"
+    (let* ((oldpaths (pg-shell-path--depathify pathvar))
+           (finalpath
+            (or (and oldpaths
+                     (pg-shell-path--pathify
+                      (list
+                       (pg-shell-path--pathify oldpaths)
+                       (pg-shell-path--pathify newpaths))))
+                (pg-shell-path--pathify newpaths))))
+      (setenv pathvar finalpath)))
 
 
-(defun pg/shell-path-remove (pathvar path)
-  "Remove PATH from PATHVAR"
-  (let* ((oldpaths (pg/--shell-depathify pathvar))
-         (finalpath (pg/--shell-pathify (remove-if
-                                         '(lambda (x) (string= x path))
-                                         oldpaths))))
-    (setenv pathvar finalpath)))
+;;;###autoload
+ (defun pg-shell-path-prepend (pathvar newpaths)
+   "Prepend list of NEWPATHS to the environment variable PATHVAR"
+   (let* ((oldpaths (pg-shell-path--depathify pathvar))
+          (finalpath
+           (or (and oldpaths
+                    (pg-shell-path--pathify
+                     (list
+                      (pg-shell-path--pathify newpaths)
+                      (pg-shell-path--pathify oldpaths))))
+               (pg-shell-path--pathify newpaths))))
+     (setenv pathvar finalpath)))
 
-(defalias 'remove-from-shell-path 'pg/shell-path-remove)
 
-(defun pg/shell-path-spec-as-lines (pathspec)
-  "Convert a colon-separated path specification to a series of
-lines for display"
-  (mapconcat (lambda (x) (concat "    " x))
-             (pg/--shell-depathify-string pathspec)
-             "\n"))
+ ;;;###autoload
+ (defun pg-shell-path-replace (pathvar newpaths)
+   "Replace PATHVAR with NEWPATHS"
+   (setenv pathvar (pg-shell-path--pathify newpaths)))
 
-(defalias 'pathspec-as-lines 'pg/shell-path-spec-as-lines)
+ ;;;###autoload
+ (defun pg-shell-path-remove (pathvar path)
+   "Remove PATH from PATHVAR"
+   (let* ((oldpaths (pg-shell-path--depathify pathvar))
+          (finalpath (pg-shell-path--pathify
+                      (remove-if
+                       '(lambda (x) (string= x path))
+                       oldpaths))))
+     (setenv pathvar finalpath)))
 
-(defun pg/shell-path-substitute (pathvar path-a path-b)
+ ;;;###autoload
+ (defun pg-shell-path-spec-as-lines (pathspec)
+   "Convert a colon-separated path specification to a series of
+ lines for display"
+   (mapconcat (lambda (x) (concat "    " x))
+              (pg-shell-path--depathify-string pathspec)
+              "\n"))
+
+(defun pg-shell-path-substitute (pathvar path-a path-b)
   "Replace instances of PATH-A in PATHVAR with PATH-B"
   (interactive "sPATHVAR: \nDDirectory to replace: \nDReplace with: ")
   (let* ((oldpaths (shell-depathify pathvar))
@@ -89,9 +88,9 @@ lines for display"
          (finalpath (shell-pathify (mapcar repl-fn oldpaths))))
     (setenv pathvar finalpath)))
 
-(defalias 'replace-in-shell-path 'pg/shell-path-substitute)
+(defalias 'replace-in-shell-path 'pg-shell-path-substitute)
 
-(defun pg/shell-path (arg pathvar newpath)
+(defun pg-shell-path (arg pathvar newpath)
   "Prepend NEWPATH to PATHVAR. If a prefix argument is specified,
 append the directory instead."
   (interactive "p\nsShell path var: \nDDirectory to add: ")
@@ -99,14 +98,10 @@ append the directory instead."
       (append-shell-path pathvar (list (expand-file-name newpath)))
     (prepend-shell-path pathvar (list (expand-file-name newpath)))))
 
-(defalias 'shell-path 'pg/shell-path)
-
-(defun print-shell-path (pathvar)
+(defun pg-shell-path-print (pathvar)
   "Print PATHVAR as a list to the console."
   (interactive "sPATHVAR: ")
   (message "%s is:\n%s" pathvar (pathspec-as-lines (getenv pathvar))))
-
-(defalias 'print-shell-path 'pg/shell-path-print)
 
 (provide 'pg-shell-path)
 ;;; pg-shell-path.el ends here
--- docs/elpa/pg-terminal-config-0.1.el
+++ docs/elpa/pg-terminal-config-0.1.el
@@ -6,39 +6,42 @@
 ;; Version: 0.1
 ;; Keywords: gui
 
-(defvar pg/t-setup-hook '()
+(defcustom pg-terminal-t-setup-hook '()
   "Run in term-setup-hook to customize Emacs run on UNIX text
   terminals (including OS X)")
-(add-hook 'pg/t-setup-hook (lambda () (message "Running pg/t-setup-hook")))
-
-(defvar pg/x-setup-hook '()
+(defcustom pg-terminal-x-setup-hook '()
   "Run in term-setup-hook to customize Emacs run on UNIX
   X-Windows (including OS X?)")
-(add-hook 'pg/t-setup-hook (lambda () (message "Running pg/x-setup-hook")))
-
-(defvar pg/w32-setup-hook '()
+(defcustom pg-terminal-w32-setup-hook '()
   "Run in term-setup-hook to customize Emacs run on
   Windows (GUI)")
-(add-hook 'pg/t-setup-hook (lambda () (message "Running pg/w32-setup-hook")))
-
-(defvar pg/ns-setup-hook '()
+(defcustom pg-terminal-ns-setup-hook '()
   "Run in term-setup-hook to customize Emacs run on OS
   X (GUI)")
-(add-hook 'pg/t-setup-hook (lambda () (message "Running pg/ns-setup-hook")))
-
-(defvar pg/pc-setup-hook '()
+(defcustom pg-terminal-pc-setup-hook '()
   "Run in term-setup-hook to customize Emacs run on Windows
   text terminals (so-called \"DOS terminals\".)")
-(add-hook 'pg/t-setup-hook (lambda () (message "Running pg/pc-setup-hook")))
 
-(add-hook 'term-setup-hook
-          (lambda ()
-            (case window-system
-              ('t (run-hooks 'pg/t-setup-hook))
-              ('x (run-hooks 'pg/x-setup-hook))
-              ('w32 (run-hooks 'pg/w32-setup-hook))
-              ('ns (run-hooks 'pg/ns-setup-hook))
-              ('pc (run-hooks 'pg/pc-setup-hook)))))
+;;;###autoload
+(defun pg-terminal-setup-hooks ()
+  (add-hook 'pg-terminal-t-setup-hook
+            (lambda () (message "Running pg-terminal-t-setup-hook")))
+  (add-hook 'pg-terminal-t-setup-hook
+            (lambda () (message "Running pg-terminal-x-setup-hook")))
+  (add-hook 'pg-terminal-t-setup-hook
+            (lambda () (message "Running pg-terminal-w32-setup-hook")))
+  (add-hook 'pg-terminal-t-setup-hook
+            (lambda () (message "Running pg-terminal-ns-setup-hook")))
+  (add-hook 'pg-terminal-t-setup-hook
+            (lambda () (message "Running pg-terminal-pc-setup-hook")))
+  (add-hook 'term-setup-hook
+            (lambda ()
+              (case window-system
+                ('t (run-hooks 'pg-terminal-t-setup-hook))
+                ('x (run-hooks 'pg-terminal-x-setup-hook))
+                ('w32 (run-hooks 'pg-terminal-w32-setup-hook))
+                ('ns (run-hooks 'pg-terminal-ns-setup-hook))
+                ('pc (run-hooks 'pg-terminal-pc-setup-hook))))))
 
 (provide 'pg-terminal-config)
 ;;; pg-terminal-config.el ends here
